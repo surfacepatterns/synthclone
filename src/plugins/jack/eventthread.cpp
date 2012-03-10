250130de2402c6eb611d6d5f723ea56dff79a2dc
Devin Anderson <surfacepatterns@gmail.com>
2012-03-10 14:31:22 -0800

    Remove debug header from EventThread class in JACK plugin.

--- src/plugins/jack/eventthread.cpp
+++ src/plugins/jack/eventthread.cpp
@@ -1,7 +1,7 @@
 /*
  * libsynthclone_jack - JACK Audio Connection Kit sampler plugin for
  * `synthclone`
- * Copyright (C) 2011 Devin Anderson
+ * Copyright (C) 2011-2012 Devin Anderson
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
@@ -20,8 +20,6 @@
 
 #include "eventthread.h"
 #include "sampler.h"
-
-#include <QtCore/QDebug>
 
 EventThread::EventThread(Sampler *sampler, QObject *parent):
     QThread(parent)
