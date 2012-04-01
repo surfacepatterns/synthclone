59b7831a8bdcce952e3def26bca3d2ab8da41088
Devin Anderson <surfacepatterns@gmail.com>
2012-03-31 22:56:52 -0700

    Add some debug code to the new SampleFile class.

--- src/lib/samplestream.cpp
+++ src/lib/samplestream.cpp
@@ -18,6 +18,7 @@
  */
 
 #include <synthclone/samplestream.h>
+#include <synthclone/util.h>
 
 #include "samplefile.h"
 
@@ -26,7 +27,7 @@ using synthclone::SampleStream;
 SampleStream::SampleStream(QObject *parent):
     QObject(parent)
 {
-    // Empty
+    file = 0;
 }
 
 SampleStream::~SampleStream()
@@ -37,53 +38,62 @@ SampleStream::~SampleStream()
 void
 SampleStream::close()
 {
+    CONFIRM(file, tr("file is set to NULL"));
     file->close();
 }
 
 synthclone::SampleChannelCount
 SampleStream::getChannels() const
 {
+    CONFIRM(file, tr("file is set to NULL"));
     return file->getChannels();
 }
 
 SampleStream::EndianType
 SampleStream::getEndianType() const
 {
+    CONFIRM(file, tr("file is set to NULL"));
     return file->getEndianType();
 }
 
 synthclone::SampleFrameCount
 SampleStream::getFrames() const
 {
+    CONFIRM(file, tr("file is set to NULL"));
     return file->getFrames();
 }
 
 synthclone::SampleRate
 SampleStream::getSampleRate() const
 {
+    CONFIRM(file, tr("file is set to NULL"));
     return file->getSampleRate();
 }
 
 SampleStream::SubType
 SampleStream::getSubType() const
 {
+    CONFIRM(file, tr("file is set to NULL"));
     return file->getSubType();
 }
 
 SampleStream::Type
 SampleStream::getType() const
 {
+    CONFIRM(file, tr("file is set to NULL"));
     return file->getType();
 }
 
 bool
 SampleStream::isClosed() const
 {
+    CONFIRM(file, tr("file is set to NULL"));
     return file->isClosed();
 }
 
 synthclone::SampleFrameCount
 SampleStream::seek(SampleFrameCount frames, Offset offset)
 {
+    CONFIRM(file, tr("file is set to NULL"));
     return file->seek(frames, offset);
 }
