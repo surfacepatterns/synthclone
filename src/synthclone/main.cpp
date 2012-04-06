8714c329452c6dcd5223651a21605502d3e6ffdf
Devin Anderson <surfacepatterns@gmail.com>
2012-04-06 00:53:10 -0700

    Expicitly unload translations before application object destruction.

--- src/synthclone/main.cpp
+++ src/synthclone/main.cpp
@@ -37,43 +37,45 @@ main(int argc, char **argv)
     Application application(argc, argv);
     QStringList arguments = application.arguments();
     QString errorMessage;
-    try {
-        application.setApplicationName("synthclone");
-        application.setOrganizationDomain("synthclone.googlecode.com");
-        application.setOrganizationName("synthclone.googlecode.com");
 
-        // Translations
-        QString directory =
-            QLibraryInfo::location(QLibraryInfo::TranslationsPath);
-        QString language = QLocale::system().name();
-        QTranslator qtTranslator;
-        qtTranslator.load("qt_" + language, directory);
-        application.installTranslator(&qtTranslator);
-        QTranslator translator;
-        translator.load("synthclone_" + language);
-        application.installTranslator(&translator);
-        qDebug() << application.tr("Translations loaded.");
+    application.setApplicationName("synthclone");
+    application.setOrganizationDomain("synthclone.googlecode.com");
+    application.setOrganizationName("synthclone.googlecode.com");
 
-        // Command line arguments
+    // Translations
+    QString directory = QLibraryInfo::location(QLibraryInfo::TranslationsPath);
+    QString language = QLocale::system().name();
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + language, directory);
+    application.installTranslator(&qtTranslator);
+    QTranslator translator;
+    translator.load("synthclone_" + language);
+    application.installTranslator(&translator);
+    qDebug() << application.tr("Translations loaded.");
 
-        bool loadSession;
-        QDir sessionDirectory;
-        switch (arguments.count()) {
-        case 0:
-        case 1:
-            loadSession = false;
+    // Command line arguments
+    bool loadSession;
+    int result;
+    QDir sessionDirectory;
+    switch (arguments.count()) {
+    case 0:
+    case 1:
+        loadSession = false;
+        break;
+    case 2:
+        if (sessionDirectory.cd(arguments[1])) {
+            loadSession = true;
             break;
-        case 2:
-            if (sessionDirectory.cd(arguments[1])) {
-                loadSession = true;
-                break;
-            }
-            // Fallthrough on purpose
-        default:
-            QTextStream(stderr, QIODevice::WriteOnly) <<
-                application.tr("Usage: synthclone [qt-args] [session-dir]");
-            return EXIT_FAILURE;
         }
+        // Fallthrough on purpose
+    default:
+        QTextStream(stderr, QIODevice::WriteOnly) <<
+            application.tr("Usage: synthclone [qt-args] [session-dir]\n");
+        result = EXIT_FAILURE;
+        goto unloadTranslations;
+    }
+
+    try {
 
         // Controller
         qDebug() << application.tr("Creating core application objects ...");
@@ -84,13 +86,25 @@ main(int argc, char **argv)
         qDebug() << application.tr("Running ...");
         controller.run(loadSession ? &sessionDirectory : 0);
 
-        qDebug() << application.tr("Exiting without errors ...");
-        return EXIT_SUCCESS;
     } catch (synthclone::Error &e) {
         errorMessage = e.getMessage();
     } catch (std::exception &e) {
         errorMessage = e.what();
     }
-    QTextStream(stderr) << application.tr("Error: %1").arg(errorMessage);
-    return EXIT_FAILURE;
+
+    // Deal with errors.
+    if (errorMessage.isEmpty()) {
+        qDebug() << application.tr("Exiting without errors ...");
+        result = EXIT_SUCCESS;
+    } else {
+        QTextStream(stderr) << application.tr("Error: %1\n").arg(errorMessage);
+        result = EXIT_FAILURE;
+    }
+
+unloadTranslations:
+    qDebug() << application.tr("Unloading translations ...");
+    application.removeTranslator(&translator);
+    application.removeTranslator(&qtTranslator);
+
+    return result;
 }
